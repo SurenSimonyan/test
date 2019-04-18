7f4a5407dbd2ce63caa703e722635e8f81aa4ed9
Suren <ssimonyan@questrade.com>
2019-04-18 17:27:07 +0400

    last changes

---
+++ script.js
@@ -0,0 +1,15 @@
+
+'use strict'
+//let answer = prompt("Are you 18?", "Yes")
+//console.log(answer)
+//console.log(typeof(null));
+let incr=10,
+    decr=10;
+
+let a= incr++;
+
+let isChecked = false,
+    isClose= false;
+    console.log(isChecked||!isClose)
+
+
